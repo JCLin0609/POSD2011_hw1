a8b832c96d4d45dc2ba9c156e4329062853b0e30
JCLin0609 <as11675847@gmail.com>
2022-12-07 10:55:13 +0800

    revise

--- test.h
+++ test.h
@@ -11,45 +11,48 @@ TEST(iPin, getOutput)
     iPin *i1 = new iPin(0);
 
     i1->setOutput(0);
-    EXPECT_EQ(i1->getOutput(),0);
+    EXPECT_EQ(i1->getOutput(), 0);
 
     i1->setOutput(1);
-    EXPECT_EQ(i1->getOutput(),1);
+    EXPECT_EQ(i1->getOutput(), 1);
 
     delete i1;
 }
 
-TEST(oPin, getOutput){
+TEST(oPin, getOutput)
+{
     iPin *i1 = new iPin(0);
     oPin *o1 = new oPin(0);
 
     o1->addInputPin(i1);
 
     i1->setOutput(0);
-    EXPECT_EQ(o1->getOutput(),0);
+    EXPECT_EQ(o1->getOutput(), 0);
 
     i1->setOutput(1);
-    EXPECT_EQ(o1->getOutput(),1);
+    EXPECT_EQ(o1->getOutput(), 1);
 
-    delete i1,o1;
+    delete i1, o1;
 }
 
-TEST(gateNot, getOutput){
+TEST(gateNot, getOutput)
+{
     iPin *i1 = new iPin(0);
     gateNot *n1 = new gateNot(0);
 
     n1->addInputPin(i1);
 
     i1->setOutput(0);
-    EXPECT_EQ(n1->getOutput(),1);
+    EXPECT_EQ(n1->getOutput(), 1);
 
     i1->setOutput(1);
-    EXPECT_EQ(n1->getOutput(),0);
+    EXPECT_EQ(n1->getOutput(), 0);
 
-    delete i1,n1;
+    delete i1, n1;
 }
 
-TEST(gateOR, getOutput){
+TEST(gateOR, getOutput)
+{
     iPin *i1 = new iPin(0);
     iPin *i2 = new iPin(1);
     gateOR *n1 = new gateOR(1);
@@ -58,24 +61,25 @@ TEST(gateOR, getOutput){
 
     i1->setOutput(0);
     i2->setOutput(0);
-    EXPECT_EQ(n1->getOutput(),0);
+    EXPECT_EQ(n1->getOutput(), 0);
 
     i1->setOutput(0);
     i2->setOutput(1);
-    EXPECT_EQ(n1->getOutput(),1);
+    EXPECT_EQ(n1->getOutput(), 1);
 
     i1->setOutput(1);
     i2->setOutput(0);
-    EXPECT_EQ(n1->getOutput(),1);
+    EXPECT_EQ(n1->getOutput(), 1);
 
     i1->setOutput(1);
     i2->setOutput(1);
-    EXPECT_EQ(n1->getOutput(),1);
+    EXPECT_EQ(n1->getOutput(), 1);
 
-    delete i1,i2,n1;
+    delete i1, i2, n1;
 }
 
-TEST(gateAND, getOutput){
+TEST(gateAND, getOutput)
+{
     iPin *i1 = new iPin(0);
     iPin *i2 = new iPin(1);
     gateAND *n1 = new gateAND(1);
@@ -84,35 +88,97 @@ TEST(gateAND, getOutput){
 
     i1->setOutput(0);
     i2->setOutput(0);
-    EXPECT_EQ(n1->getOutput(),0);
+    EXPECT_EQ(n1->getOutput(), 0);
 
     i1->setOutput(0);
     i2->setOutput(1);
-    EXPECT_EQ(n1->getOutput(),0);
+    EXPECT_EQ(n1->getOutput(), 0);
 
     i1->setOutput(1);
     i2->setOutput(0);
-    EXPECT_EQ(n1->getOutput(),0);
+    EXPECT_EQ(n1->getOutput(), 0);
 
     i1->setOutput(1);
     i2->setOutput(1);
-    EXPECT_EQ(n1->getOutput(),1);
+    EXPECT_EQ(n1->getOutput(), 1);
 
-    delete i1,i2,n1;
+    delete i1, i2, n1;
 }
 
-TEST(LogicSim, load){
+TEST(LogicSim, load)
+{
     string path = "File1.txt";
 
     LogicSim *t = new LogicSim;
-    EXPECT_EQ(t->load(path),true);
+    EXPECT_EQ(t->load(path), true);
+
+    delete t;
 }
 
-TEST(LogicSim, getTruthTable){
+TEST(LogicSim, getSimulationResult)
+{
     string path = "File1.txt";
     LogicSim *t = new LogicSim;
 
     t->load(path);
-    EXPECT_EQ(t->getTruthTable(),"0 0 0 0 1 1 0 0 ");
+
+    vector<int> input;
+    input.push_back(0);
+    input.push_back(0);
+    input.push_back(0);
+    EXPECT_EQ(t->getSimulationResult(input), "0");
+
+    input[0] = 0;
+    input[1] = 0;
+    input[2] = 1;
+    EXPECT_EQ(t->getSimulationResult(input), "0");
+
+    input[0] = 0;
+    input[1] = 1;
+    input[2] = 0;
+    EXPECT_EQ(t->getSimulationResult(input), "0");
+
+    input[0] = 0;
+    input[1] = 1;
+    input[2] = 0;
+    EXPECT_EQ(t->getSimulationResult(input), "0");
+
+    input[0] = 0;
+    input[1] = 1;
+    input[2] = 1;
+    EXPECT_EQ(t->getSimulationResult(input), "0");
+
+    input[0] = 1;
+    input[1] = 0;
+    input[2] = 0;
+    EXPECT_EQ(t->getSimulationResult(input), "1");
+
+    input[0] = 1;
+    input[1] = 0;
+    input[2] = 1;
+    EXPECT_EQ(t->getSimulationResult(input), "1");
+
+    input[0] = 1;
+    input[1] = 1;
+    input[2] = 0;
+    EXPECT_EQ(t->getSimulationResult(input), "0");
+
+    input[0] = 1;
+    input[1] = 1;
+    input[2] = 1;
+    EXPECT_EQ(t->getSimulationResult(input), "0");
+
+    delete t;
+}
+
+TEST(LogicSim, getTruthTable)
+{
+    string path = "File1.txt";
+    LogicSim *t = new LogicSim;
+
+    t->load(path);
+    EXPECT_EQ(t->getTruthTable(), "0 0 0 0 1 1 0 0 ");
+
+    delete t;
 }
 #endif
